4f9f7f4b65c6bc8be5bd5e7968b9038ea79e49a1
Ekaterina <tebeneklitsy@mail.ru>
2019-03-10 14:27:08 +0300

    Commit lab2 done

--- homeworks/HW2/src/main/java/ru/example/BullsAndCows.java
+++ homeworks/HW2/src/main/java/ru/example/BullsAndCows.java
@@ -1,12 +1,49 @@
 package ru.example;
 import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Random;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class BullsAndCows {
+
+    private static String timeStamp = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());
+    private static final Logger LOGGER = Logger.getLogger(BullsAndCows.class.getName());
+    private static FileHandler fh;
+
     public static void main(String[] args) {
+
+        try {
+            boolean flag = true;
+
+            fh = new FileHandler("logfile_" + timeStamp + ".log");
+            LOGGER.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+            System.out.println("Welcome to Bulls and Cows game!");
+            LOGGER.info( "Logger started!" );
+
+            do{
+                flag = playGame();
+            }
+            while (flag);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static boolean playGame(){
 
         Scanner in = new Scanner(System.in);
         int [] bullsAndCows;
@@ -14,36 +51,47 @@ public class BullsAndCows {
         Random Random = new Random();
         int countOfAttempt = 10;
         //String word = "java";     //для тестирования
-
         String word = getWordFromFile(Random.nextInt(52976));
-        System.out.println("Welcome to Bulls and Cows game! I offered a " + word.length() + "-letter word, your guess?");
+
+        //System.out.println("I offered a " + word.length() + "-letter word, your guess?");
+        LOGGER.info("I offered a " + word.length() + "-letter word, your guess?" );
 
         for (int i = 0; i < countOfAttempt; i++)
         {
             userSting = in.nextLine();
+
+            if (userSting.length() != word.length()) {
+                //System.out.println("Wrong word length! Launch a new game!");
+                LOGGER.info("Wrong word length! Launch a new game!" );
+                return true;
+            }
+
             bullsAndCows = checkBullsAndCows(userSting, word);
 
             if (bullsAndCows[0] == word.length()){
-                System.out.println("You won!");
+                //System.out.println("You won!");
+                LOGGER.info("You won!" );
                 i = countOfAttempt - 1;
             }
             else {
-                System.out.println("Bulls: " + bullsAndCows[0] + ";  Cows: " + bullsAndCows[1]);
+                //System.out.println("Bulls: " + bullsAndCows[0] + ";  Cows: " + bullsAndCows[1]);
+                LOGGER.info("Bulls: " + bullsAndCows[0] + ";  Cows: " + bullsAndCows[1]);
                 if (i == countOfAttempt - 1) {
-                    System.out.println("You lose!");
+                    //System.out.println("You lose! Word is: " + word);
+                    LOGGER.info("You lose! Word is: " + word );
                 }
             }
 
             if (i == countOfAttempt - 1) {
                 System.out.println("Wanna play again? yes/no");
+                //LOGGER.info("Wanna play again? yes/no" );
                 if (in.nextLine().equals("yes")) {
-                    word = getWordFromFile(Random.nextInt(52976));
-                    System.out.println("I offered a " + word.length() + "-letter word, your guess?");
-                    i = -1;
+                    return true;
                 }
             }
         }
 
+        return false;
     }
 
     public static String getWordFromFile(int rand){
--- homeworks/HW2/src/test/java/ru/example/Tests.java
+++ homeworks/HW2/src/test/java/ru/example/Tests.java
@@ -1,15 +1,28 @@
 package ru.example;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
 import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
 
 public class Tests {
 
     @Test
-    public void max0() throws Exception {
-        assertEquals("aahed", BullsAndCows.getWordFromFile(0));
+    public void getWordFromFile() throws Exception {
+        assertEquals("aaliis", BullsAndCows.getWordFromFile(4));
+    }
+
+    @Test
+    public void checkBulls() throws Exception {
+        String word = "java";
+        String userWord = "lava";
+
+        assertEquals(3, BullsAndCows.checkBullsAndCows(userWord, word)[0]);
+    }
+
+    @Test
+    public void checkCows() throws Exception {
+        String word = "java";
+        String userWord = "lava";
+
+        assertEquals(0, BullsAndCows.checkBullsAndCows(userWord, word)[1]);
     }
 }
