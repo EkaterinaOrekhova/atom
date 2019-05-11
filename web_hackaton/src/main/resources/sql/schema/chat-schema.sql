39f860c37bcae97d13886a2a26f8c72d341df246
Ekaterina <tebeneklitsy@mail.ru>
2019-05-11 13:42:40 +0300

    fix tasks

--- web_hackaton/src/main/resources/sql/schema/chat-schema.sql
+++ web_hackaton/src/main/resources/sql/schema/chat-schema.sql
@@ -1,18 +1,15 @@
 begin;
 
-drop schema if exists chat cascade;
-create schema chat;
+create schema if not exists chat;
 
-drop table if exists chat.user;
-create table chat.user (
+create table if not exists chat.user (
   id    serial             not null,
   login varchar(20) unique not null,
 
   primary key (id)
 );
 
-drop table if exists chat.message;
-create table chat.message (
+create table if not exists chat.message (
   id     serial       not null,
   user_id integer     not null references chat.user on delete cascade,
   time   timestamp    not null,
